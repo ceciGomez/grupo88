7e997d6ddbee6303ed37d6ed88dcd68ed9e8c643
ceciliadrunk <cecilia.mlgz@gmail.com>
2015-09-05 23:01:27 -0300

    actualización de sabado

---
+++ application/views/pages/paso4.php
@@ -0,0 +1,90 @@
+<div class="container">
+<p>Verficar Frascos con Codigos de Barras</p>
+<div class="table-responsive">
+
+<table class="table table-hover">
+
+
+<!-- On cells (`td` or `th`) -->
+<tr>
+  <td class="success">Frascos ID</td>
+  <td class="success">Codigo de Barras</td>
+  <td class="success">Cambiar de Frasco</td>
+ 
+</tr>
+
+<tr>
+  <td class="active">...</td>
+  <td class="active">
+     <button class="btn btn-success"  type="submit" btn-lg  >Leer/Reintentar</button>
+      <form class="form-inline" role="form">
+       <div class="form-group">
+            <input type="text" class="form-control" placeholder="x xxxxxx xxxxxxx">
+       </div>
+     </form>
+  </td>
+  <td class="active">
+    <button class="btn btn-danger"  type="submit" btn-lg  >Cambiar</button>
+  </td>
+  
+ 
+</tr>
+<tr>
+  <td class="active">...</td>
+  <td class="active">
+     <button class="btn btn-success"  type="submit" btn-lg  >Leer/Reintentar</button>
+     <form class="form-inline" role="form">
+       <div class="form-group">
+            <input type="text" class="form-control" placeholder="x xxxxxx xxxxxxx">
+       </div>
+     </form>
+  </td>
+  <td class="active">
+    <button class="btn btn-danger"  type="submit" btn-lg  >Cambiar</button>
+  </td>
+ 
+  
+ 
+</tr>
+<tr>
+  <td class="active">...</td>
+  <td class="active">
+     <button class="btn btn-success"  type="submit" btn-lg  >Leer/Reintentar</button>
+      <form class="form-inline" role="form">
+       <div class="form-group">
+            <input type="text" class="form-control" placeholder="x xxxxxx xxxxxxx">
+       </div>
+     </form>
+  </td>
+  <td class="active">
+    <button class="btn btn-danger"  type="submit" btn-lg  >Cambiar</button>
+  </td>
+ 
+  
+ 
+</tr>
+<tr>
+  <td class="active">...</td>
+  <td class="active">
+     <button class="btn btn-success"  type="submit" btn-lg  >Leer/Reintentar</button>
+      <form class="form-inline" role="form">
+       <div class="form-group">
+            <input type="text" class="form-control" placeholder="x xxxxxx xxxxxxx">
+       </div>
+     </form>
+  </td>
+  <td class="active">
+    <button class="btn btn-danger"  type="submit" btn-lg  >Cambiar</button>
+  </td>
+  
+  
+ 
+
+</table>
+
+<div class="pull-right">
+  <a href="<?php echo base_url();?>index.php/page/view/paso5" class="btn btn-primary btn-lg active" role="button">Generar etiquetas</a>
+
+</div>
+</div>
+</div>
