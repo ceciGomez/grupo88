57572e314cf6bc2f3832dc389cc00d8aafe6c149
ceciliadrunk <cecilia.mlgz@gmail.com>
2015-09-12 11:04:02 -0300

    añadido controlador consentimiento

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,7 @@
 <aside class="right-side">
 <div class="container"> 
 	<img src="<?php echo base_url();?>assets/internals/img/banco.jpg" 
-	class="img-responsive" alt="Cinque Terre" height="200px" width="350px" align="center">
+	class="img-responsive" alt="Cinque Terre" height="auto" width="auto" align="center">
 </div> 
 
 </aside>
