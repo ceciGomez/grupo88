019460485a257ec527b85f150c1ab82b5c8d6dc1
evelinB <evelin.anali@gmail.com>
2015-11-08 22:04:51 -0300

    controller frascos

---
+++ application/controllers/Cfrascos.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cfrascos extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Cfrascos.php */
+/* Location: ./application/controllers/Cfrascos.php */
