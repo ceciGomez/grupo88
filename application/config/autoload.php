d24a4b5c36ad8421ebbf262027b8a2387647200f
ceciliadrunk <cecilia.mlgz@gmail.com>
2016-01-10 21:30:52 -0300

    autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -141,4 +141,4 @@ $autoload['model'] = array('donantes_model', 'consentimiento_model', 'bebeasocia
 	'bebereceptor_model', 'serologia_model','seguimientoba_model',
 	'hojaruta_model', 'zona_model', 'reporte_model', 'frascos_model',
 	'localidad_model', 'provincia_model', 'fraccionamiento_model', 'medios_model',
-	'pasteurizacion_model', 'pmedica_model');
+	'pasteurizacion_model', 'pmedica_model', 'insumos_model');
