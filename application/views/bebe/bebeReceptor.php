d4877a26eb4fbb723a65a0cd5b514556d92f0db1
ElisaGit <gut.elisa@gmail.com>
2016-01-15 21:29:57 -0300

    baslasla

--- application/views/bebe/bebeReceptor.php
+++ application/views/bebe/bebeReceptor.php
@@ -1,4 +1,4 @@
-<!--Comentario de push ->
+<!--Comentario de push -->
 <!-- Right side column. Contains the navbar and content of the page -->
 <aside class="right-side">
  <!-- Content Header (Page header) -->
