8bbaa016374ddff2ff67869e57408eeec52e4d66
ceciliadrunk <cecilia.mlgz@gmail.com>
2016-02-22 23:57:59 -0300

    pasteurizacionPDF

--- pasteurizacionPDF.php
+++ pasteurizacionPDF.php
@@ -1,6 +1,6 @@
 <?php 
 require('fpdf.php');
-require('conexionRepor.php');
+require('conexion.php');
 //$GET['idPa'] =  1;
 $GLOBALS['paste'] = $_GET['idPa'];
 $GLOBALS['responsable'] = 'Cuzziol, Eric';
