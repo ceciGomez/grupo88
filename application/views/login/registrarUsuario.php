8ca1d93116b8e3aaec83c51e347a1a43644666c4
ElisaGit <gut.elisa@gmail.com>
2016-02-24 01:48:48 -0300

    listo doble validacion de pass

--- application/views/login/registrarUsuario.php
+++ application/views/login/registrarUsuario.php
@@ -9,50 +9,63 @@
    <section class="content">
       <div class="container-fluid">
          <div class="row">
-            <div class="col-md-6 col-md-offset-2">
-               <div class="panel panel-default">
+            <div class="col-md-8 panel panel-default col-md-offset-2">
                   <div class="panel-body">
-                     <form data-toggle="validator" class="form-horizontal" role="form" action="<?php echo base_url() ?>index.php/clogin/altaUsuario" method="post" name="formRegistrarUs" id="formRegistrarUsuario" accept-charset="utf-8">
+                     <form style="margin: auto" data-toggle="validator" class="form-horizontal" role="form" action="<?php echo base_url() ?>index.php/clogin/altaUsuario" method="post" name="formRegistrarUs" id="formRegistrarUsuario" accept-charset="utf-8">
                         <div class="row">
-                           <div class="col-md-6">
+                           <div class="form-group col-md-6">
+                              <div>
                               <label >Nombre</label>
                               <input type="text" id="nombre" class="form-control" placeholder="Escriba el Nombre aqui" name="nombre" required/>
                               <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                               <span class="help-block with-errors">* Campo Requerido</span>
+                              </div>
                            </div>
-                           <div class="col-md-6">
+                           <div class="form-group col-md-6">
+                              <div>
                               <label >Apellido</label>
                               <input type="text" id="apellido" class="form-control" placeholder="Escriba el Apellido aqui" name="apellido" required/>
                               <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                               <span class="help-block with-errors">* Campo Requerido</span>
+                              </div>
                            </div>
-                           <div class="col-md-6">
+                           <div class=" form-group col-md-6">
+                              <div>
                               <label >Email</label>
-                              <input type="mail" id="mail" class="form-control" placeholder="Escriba el Nombre aqui" name="mail"/>
+                              <input type="email" id="mail" class="form-control" placeholder="Escriba el Nombre aqui" name="mail" required/>
+                              </div>
                            </div>
-                           <div class="col-md-6">
+                           <div class="form-group col-md-6">
+                              <div>
                               <label >Nombre Usuario</label>
-                              <input type="text" id="nomUsuario" class="form-control" placeholder="Escriba el Nombre de usuario aqui" name="nomUsuario" required/>
+                              <input data-error="este campo no puede estar vacio" type="text" id="nomUsuario" class="form-control" placeholder="Escriba el Nombre de usuario aqui" name="nomUsuario" required/>
                               <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                               <span class="help-block with-errors">* Campo Requerido</span>
+                              </div>
                            </div>
-                           <div class="col-md-6">
+                           <div class="form-group col-md-6">
+                              <div>
                               <label >Contraseña</label>
-                              <input type="password" id="pass" class="form-control" placeholder="Escriba contraseña aqui" name="pass" required/>
+                              <input type="password" data-minlength="6" id="pass" class="form-control" placeholder="Escriba contraseña aqui" name="pass" required/>
                               <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                               <span class="help-block with-errors">* Campo Requerido</span>
+                              </div>
                            </div>
-                           <div class="col-md-6">
+                           <div class="form-group col-md-6">
+                              <div>
                               <label >Repita su Contraseña</label>
-                              <input type="password" id="pass2" class="form-control" placeholder="Escriba contraseña aqui" name="pass2" required/>
+                              <input type="password" id="pass2" class="form-control" data-match="#inputPassword" data-match-error="las contraseñas no son iguales" placeholder="Vuelva a escribir la contraseña" name="pass2" required/>
                               <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                               <span class="help-block with-errors">* Campo Requerido</span>
+                              </div>
                            </div>
-                           <div class="col-md-6">
+                           <div class="form-group col-md-6">
+                              <div>
                               <label >Tipo de Usuario</label>
                               <input type="text" id="tipo" class="form-control" placeholder="Escriba tipo usuario aqui" name="tipo" required/>
                               <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                               <span class="help-block with-errors">* Campo Requerido</span>
+                              </div>
                            </div>
                         </div>
                         <br>
@@ -61,7 +74,6 @@
                         </div>
                      </form>
                   </div>
-               </div>
             </div>
          </div>
       </div>
@@ -69,3 +81,6 @@
 </aside>
 <!-- /.right-side -->
 <script src="<?php echo base_url();?>assets/internals/js/logininfo.js" type="text/javascript" charset="utf-8" async defer></script>
+<script type="text/javascript">
+$('#formRegistrarUs').validator()
+</script>
